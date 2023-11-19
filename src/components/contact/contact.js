0b71837cf799ccd2a677fdb737c9d363ef21696b
Maikon Schafranski <maikonschfranski12@gmail.com>
2023-11-19 18:54:48 -0300

    commit: finalizaco completo do componente contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -11,9 +11,9 @@ import rec from "../../assets/react.png";
 import spring from "../../assets/icons8-spring-boot-480.png";
 import typeScript from "../../assets/typescript.png";
 
-import instagram from "../../assets/instagram.png";
-import facebook from "../../assets/facebook.png";
-import github from "../../assets/github.png";
+import instagram from "../../assets/instagram-colorido.png";
+import facebook from "../../assets/facebook-colorido.png";
+import github from "../../assets/25231.png";
 
 const Contact = () => {
   return (
@@ -50,11 +50,7 @@ const Contact = () => {
           <button className="submitBtn" value="send" type="submit">
             Enviar
           </button>
-          <div className="links">
-            <img src={facebook} alt="facebook" className="link" />
-            <img src={instagram} alt="instagram" className="link" />
-            <img src={github} alt="github" className="link" />
-          </div>
+         
         </form>
       </div>
     </section>
