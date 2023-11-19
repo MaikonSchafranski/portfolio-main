2cf8cff90301eb85b7f0151e2c8ee019976f57a3
Maikon Schafranski <maikonschfranski12@gmail.com>
2023-11-18 23:17:03 -0300

    commit: criacao do componente contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
+import instagram from "../../assets/instagram.png";
+import facebook from "../../assets/facebook.png";
+import github from "../../assets/github.png";
 
-const Contact = ()=> {
-    return (
-        <div className="contact">
-            <h1>Contact</h1>
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1 className="contactPageTitle">Contrate-me</h1>
+      <span className="contactDesc">
+        Preencha o formulário abaixo para discutir quaisquer oportunidades de
+        trabalho.
+      </span>
+      <form className="contactForm">
+        <input type="text" className="name" placeholder="Nome" />
+        <input type="email" className="email" placeholder="Email" />
+        <textarea
+          name="message"
+          className="msg"
+          rows="5"
+          placeholder="Mensagem"
+        ></textarea>
+        <button className="submitBtn" value="Send" type="submit">
+          Enviar
+        </button>
+        <div className="links">
+          <img className="link" src={instagram} alt="instagram" />
+          <img className="link" src={facebook} alt="facebook" />
+          <img className="link" src={github} alt="github" />
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
