79b8f265eae65bbeb6e9a8cea3aa2348abcde5c6
Maikon Schafranski <maikonschfranski12@gmail.com>
2023-11-18 23:29:34 -0300

    commit: ajuste no componente contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,31 +5,7 @@ import github from "../../assets/github.png";
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <h1 className="contactPageTitle">Contrate-me</h1>
-      <span className="contactDesc">
-        Preencha o formulário abaixo para discutir quaisquer oportunidades de
-        trabalho.
-      </span>
-      <form className="contactForm">
-        <input type="text" className="name" placeholder="Nome" />
-        <input type="email" className="email" placeholder="Email" />
-        <textarea
-          name="message"
-          className="msg"
-          rows="5"
-          placeholder="Mensagem"
-        ></textarea>
-        <button className="submitBtn" value="Send" type="submit">
-          Enviar
-        </button>
-        <div className="links">
-          <img className="link" src={instagram} alt="instagram" />
-          <img className="link" src={facebook} alt="facebook" />
-          <img className="link" src={github} alt="github" />
-        </div>
-      </form>
-    </div>
+    
   );
 };
 
