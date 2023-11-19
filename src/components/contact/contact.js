e85e8510fe1cbb36001bd93906a437a27925c9ff
Maikon Schafranski <maikonschfranski12@gmail.com>
2023-11-19 01:08:17 -0300

    commit: termino da parte 1 do componente contact e alguns ajustes em outros componentes

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,7 +5,13 @@ import github from "../../assets/github.png";
 
 const Contact = () => {
   return (
-    
+    <section className="contactPage">
+      <div id="knowledge">
+        <h1 className="contactPageTitle">Conhecimentos</h1>
+        <p className="knowledgeDesc"></p>
+      </div>
+      <div id="contact"></div>
+    </section>
   );
 };
 
